7179f6e86a62cfc529bad8c32151b1b0ce319998
Jacob Ritorto <j.ritorto@modcloth.com>
2013-01-03 18:19:30 -0500

    remove dep from recipe and put in metadata

--- cookbooks/std_oracle_projects/metadata.rb
+++ cookbooks/std_oracle_projects/metadata.rb
@@ -4,3 +4,4 @@ license          "All rights reserved"
 description      "Installs/Configures std_oracle_projects"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+require "std_oracle_groups"

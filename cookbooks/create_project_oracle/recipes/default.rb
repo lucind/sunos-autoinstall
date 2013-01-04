6be144becef27644e96aac34cf1e8c9097bc6458
Jacob Ritorto <j.ritorto@modcloth.com>
2013-01-04 14:19:19 -0500

    clean up naming

--- cookbooks/create_project_oracle/recipes/default.rb
+++ cookbooks/create_project_oracle/recipes/default.rb
@@ -1,16 +1,16 @@
-# Cookbook Name:: create_oracle_database_role
+# Cookbook Name:: create_project_oracle
 # Recipe:: default
 # Copyright (c) 2013 ModCloth. Licensed under the Apache License,  Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
-include_recipe "std_oracle_projects"
+include_recipe "create_groups_oracle"
 
-bash "create the oracle database role" do
+bash "create_project_oracle" do
   user "root"
   code <<-EOH
 
-    NAME=oracle
+    NAME=oracleproject
     ID=$( echo $(( 0x`echo -n $NAME |digest -a sha1 |cut -b1-4` )) )
-    (rolemod -K type=role $NAME $> /dev/null) || roleadd -u $ID -g oinstall -G dba,oper -K  project=oracleproject -K roleauth=user -m $NAME
+    (projmod -K "" $NAME &>/dev/null) || projadd  -p $ID -G oinstall -c "Oracle Project" $NAME
 
   EOH
 end

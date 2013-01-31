6d22ddb0a98003eeeaa611855173e89c587346d3
Jacob Ritorto <j.ritorto@modcloth.com>
2013-01-31 21:49:11 +0000

    make check a function and little details

--- solaris11autoinstallserversetup.sh
+++ solaris11autoinstallserversetup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -o verbose
-set -x
 # -- solaris 11 ai sever setup --
 
 NAME=thlayli
@@ -9,8 +8,19 @@ DOMAIN=ebs.modcloth.com
 DNSSERVER=10.1.5.21
 CLIENTIPSTART=10.1.87.101
 CLIENTIPNUM=99
+FORCE=$1 
 
-svcs -xv|ggrep -E --color  "(.*)|$"; if [ $? = 0 ] ;then echo "Kindly attend to your disconsolate services, as listed heretofore, prior to calling this script."; exit 1;fi
+function svcck {
+  true
+  while [ $? = 0 ]; do
+    if [ "$FORCE" = "-f" ]; then break; fi
+    echo "Waiting a second for services to online.  To force, include -f arg."
+    sleep 1
+    svcs -xv|ggrep -E --color  "(.*)|$"
+  done
+}
+
+svcck
 
 #identity and dns stuff
 svcadm disable -s /network/dns/multicast
@@ -21,14 +31,15 @@ ipadm create-addr -a $ADDR/24 net0
 echo "$ADDR $NAME.$DOMAIN $NAME" >>/etc/hosts
 route -p add default $ADDR
 
-svcs -xv|ggrep -E --color  "(.*)|$"
-if [ $? = 0 ] ;then exit 1;fi
+svcck
 
 svcadm disable -ts system/name-service/switch
 svccfg -s system/name-service/switch setprop 'config/host = astring: "files dns mdns"'
 #svccfg -s system/name-service/switch refresh
 svcadm refresh system/name-service/switch 
 svcadm enable -rs system/name-service/switch 
+
+svcck
 
 svcadm disable -ts network/dns/client
 svccfg -s network/dns/client setprop "config/search = astring: \"$DOMAIN\""
@@ -37,6 +48,9 @@ svccfg -s network/dns/client setprop "config/nameserver = net_address: ($DNSSERV
 svcadm refresh network/dns/client 
 svcadm enable -rs network/dns/client 
 nscfg export svc:/network/dns/client
+svcs -xv|ggrep -E --color  "(.*)|$"
+
+svcck
 
 svcadm disable -ts system/identity:node
 svccfg -s system/identity:node setprop config/nodename = astring: \"$NAME\"
@@ -44,9 +58,9 @@ svccfg -s system/identity:node setprop config/loopback = astring: \"$NAME\"
 #svccfg -s system/identity:node refresh
 svcadm refresh system/identity:node  
 svcadm enable -rs system/identity:node  
-
 svcs -xv|ggrep -E --color  "(.*)|$"
-if [ $? = 0 ] ;then exit 1;fi
+
+svcck
 
 zfs create -o atime=off rpool/export/repoSolaris11
 pkgrepo create /export/repoSolaris11
@@ -61,19 +75,27 @@ svccfg -s application/pkg/server setprop pkg/readonly=false
 svcadm disable -ts application/pkg/server
 svcadm refresh application/pkg/server
 svcadm enable -rs application/pkg/server
+
+svcck
+
 pkgsend publish -d firstboot/proto -s http://localhost firstboot/firstboot.p5m
 
 #Enable Multicast DNS
 svcadm enable -rs /network/dns/multicast
+
+svcck
 
 #Install Install Service
 pkg set-publisher -g http://pkg.oracle.com/solaris/release solaris
 pkg install install/installadm
 zfs create rpool/export/auto_install
 installadm create-service -i $CLIENTIPSTART -c $CLIENTIPNUM -a sparc -y
+
+svcck
+
 zfs snapshot rpool/export/auto_install@fresh
 installadm create-manifest -n default-sparc -f manifest/frith.xml -c mac=0:14:4f:ae:1b:7c
 installadm create-manifest -n default-sparc -f manifest/frith.xml -c mac=0:14:4f:ae:1b:7c
 installadm create-profile -n default-sparc -f sc_profiles/inle.xml -c mac=0:14:4f:e5:cd:9c
 installadm create-profile -n default-sparc -f sc_profiles/inle.xml -c mac=0:14:4f:e5:cd:9c
-installadm -cmp
+installadm list -cmp

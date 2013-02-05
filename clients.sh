b3962f01bc3656592a692e73614c37b246935b55
Jacob Ritorto <j.ritorto@modcloth.com>
2013-02-05 17:36:50 +0000

    correct client reg

--- clients.sh
+++ clients.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 installadm create-manifest -n default-sparc -f manifest/frith.xml -c mac=0:14:4f:ae:1b:7c
-installadm create-manifest -n default-sparc -f manifest/frith.xml -c mac=0:14:4f:ae:1b:7c
-installadm create-profile -n default-sparc -f sc_profiles/inle.xml -c mac=0:14:4f:e5:cd:9c
+installadm create-manifest -n default-sparc -f manifest/inle.xml -c mac=0:14:4f:e5:cd:9c
+installadm create-profile -n default-sparc -f sc_profiles/frith.xml -c mac=0:14:4f:ae:1b:7c
 installadm create-profile -n default-sparc -f sc_profiles/inle.xml -c mac=0:14:4f:e5:cd:9c
 installadm list -cmp

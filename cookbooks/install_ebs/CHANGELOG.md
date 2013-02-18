6a4c72f2862579b66a2ea563b8ecf81f54abb252
Jacob Ritorto <j.ritorto@modcloth.com>
2013-02-18 12:18:21 -0500

    initial draft of ebs install automation logic

---
+++ cookbooks/install_ebs/CHANGELOG.md
@@ -0,0 +1,12 @@
+# CHANGELOG for install_ebs
+
+This file is used to list changes made in each version of install_ebs.
+
+## 0.1.0:
+
+* Initial release of install_ebs
+
+- - - 
+Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
+
+The [Github Flavored Markdown page](http://github.github.com/github-flavored-markdown/) describes the differences between markdown on github and standard markdown.
---
+++ cookbooks/install_ebs/recipes/default.rb
@@ -0,0 +1,38 @@
+# Cookbook Name:: install_ebs
+# Recipe:: default
+# Copyright 2013, ModCloth
+# Copyright (c) 2013 ModCloth. Licensed under the Apache License,  Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+template "/export/home/oracle/config.xml" do
+  source "config.xml.erb"
+  mode 770
+  owner "oracle"
+  group "oinstall"
+end
+bash "downgrade unzip to appease oracle install script" do
+  user "root"
+  code <<-EOH
+set -o verbose
+mv /usr/bin/unzip /usr/bin/unzip.orig
+ln -s /net/thlayli/rpool/StageR12/startCD/Disk1/rapidwiz/unzil/Solaris/unzip /usr/bin/unzip
+EOH
+end
+
+bash "install_ebs" do
+  user "oracle"
+  code <<-EOH
+set -o verbose
+cd /net/thlayli/rpool/StageR12/startCD/Disk1/rapidwiz
+
+./rapidwiz -config ~oracle/config.xml -silent
+EOH
+end
+
+bash "replace original unzip" do
+  user "root"
+  only_if "ls /usr/bin/unzip.orig"
+  code <<-EOH
+set -o verbose
+rm /usr/bin/unzip
+mv /usr/bin/unzip.orig /usr/bin/unzip
+EOH
+end
